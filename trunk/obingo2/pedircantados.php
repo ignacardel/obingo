03bd2c2e1d8c6ec8fa0311a8470a3eb0d4618823
venezuelanpride23 <venezuelanpride23@4733d3b4-271b-336c-8cfb-490d029273bf>
2011-07-04 05:37:43 +0000

    pedircantados en arreglo

--- trunk/obingo2/pedircantados.php
+++ trunk/obingo2/pedircantados.php
@@ -3,9 +3,20 @@
 //include "conexion.php";
 
 
-$filename = 'cantado.txt';
+//$filename = 'cantado.txt';
+//
+//$response = file_get_contents($filename);
 
-$response = file_get_contents($filename);
+$fh = fopen("cantado.txt", "r");
+if ($fh) {
+    while (!feof($fh)) {
+        $contenido = fgets($fh);
+        if ($contenido != false) {
+            $response[] = $contenido;
+        }
+    }
+    fclose($fh);
+}
 echo json_encode($response);
 
 //include "cerrar_conexion.php";
